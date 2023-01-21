4cfa87ef1ce5d316f2d9068a307c3f0d89761416
Haruki Sao <56357240+harusys@users.noreply.github.com>
2023-01-21 16:21:55 +0000

    347. Top K Frequent Elements

---
+++ leetcode.com/medium-347.top-k-frequent-elements.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=347 lang=python3
+#
+# [347] Top K Frequent Elements
+#
+
+# @lc code=start
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return [k for k, v in Counter(nums).most_common(k)]
+# @lc code=end
